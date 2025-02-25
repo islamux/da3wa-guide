51d2b504f4a177401e22e063ae2f66688aa1c1d5
islamux <fathi733l@gmail.com>
2025-02-25 17:42:51 +0300

    make all pages to rtl, fix facebook, youtube link

--- comparisons/models.py
+++ comparisons/models.py
@@ -4,6 +4,8 @@ class Religion(models.Model):
     name = models.CharField(max_length=100)  # اسم الدين
     principles = models.TextField()         # مبادئ الدين
     comparison_notes = models.TextField()   # ملاحظات المقارنة
+    facebook_link = models.URLField(blank=True, null=True)  # رابط الفيسبوك
+    youtube_link = models.URLField(blank=True, null=True)   # رابط اليوتيوب
 
     def __str__(self):
         return self.name
