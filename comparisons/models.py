864ff229497f172112ccf6411cac0e604ce1c8ea
islamux <fathi733l@gmail.com>
2025-02-26 17:45:00 +0300

    improve ui of each sub pages, fix ulrs

--- comparisons/models.py
+++ comparisons/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 class Religion(models.Model):
     name = models.CharField(max_length=100)  # اسم الدين
-    principles = models.TextField()         # مبادئ الدين
-    comparison_notes = models.TextField()   # ملاحظات المقارنة
     facebook_link = models.URLField(blank=True, null=True)  # رابط الفيسبوك
     youtube_link = models.URLField(blank=True, null=True)   # رابط اليوتيوب
 
